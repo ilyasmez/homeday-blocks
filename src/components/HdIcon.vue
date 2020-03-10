3d050b23c115fe6895eb0168353d736ae3b78d0e
ilyasmez <ilyas@amezouar.com>
2020-03-10 19:46:38 +0100

    WIP 2

--- src/components/HdIcon.vue
+++ src/components/HdIcon.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+import _intersection from 'lodash/intersection';
 import InlineSvg from 'vue-inline-svg';
 
 export default {
@@ -15,21 +16,59 @@ export default {
     InlineSvg,
   },
   props: {
-    colors: {
+    rotate: {
+      type: Number,
+      default: 0,
+    },
+    fillFromClass: {
       type: Object,
-      default: () => ({}),
+      default: null,
+    },
+    // To add class
+    classFromFill: {
+      type: Object,
+      default: null,
     },
   },
   methods: {
     transform(svg) {
+      if (!this.fillFromClass && !this.classFromFill) {
+        return svg;
+      }
+
       const paths = svg.querySelectorAll('path');
       paths.forEach((path) => {
-        const className = path.getAttribute('class');
-        const fill = this.colors[className];
-        path.setAttribute('fill', fill);
+        const newFill = this.getFillFromClassNames(path.getAttribute('class'));
+        if (newFill) {
+          path.setAttribute('fill', newFill);
+        }
+
+        const newClassName = this.getClassNameFromFill(path.getAttribute('fill'));
+        if (newClassName) {
+          path.setAttribute('class', `${path.getAttribute('class')} ${newClassName}`);
+        }
       });
 
       return svg;
+    },
+    getFillFromClassNames(classNames) {
+      if (!classNames || !this.fillFromClass) {
+        return null;
+      }
+
+      const classNameMatches = _intersection(
+        (classNames || '').split(' '),
+        Object.keys(this.fillFromClass),
+      );
+
+      return this.fillFromClass[classNameMatches[0]];
+    },
+    getClassNameFromFill(fill) {
+      if (!fill || !this.classFromFill) {
+        return '';
+      }
+
+      return this.classFromFill[fill];
     },
   },
 };
--- src/stories/HdIcon.stories.js
+++ src/stories/HdIcon.stories.js
@@ -2,7 +2,11 @@
 import { storiesOf } from '@storybook/vue';
 import { HdIcon } from 'homeday-blocks';
 import bigIcon from 'homeday-blocks/src/assets/big-icons/ic_apartment.svg';
-import multiColorIcon from 'homeday-blocks/src/assets/multi-colors/ic_chimney.svg';
+import multiColorIcon from './assets/ic_multi-color.svg';
+import multiColorIconWithClasses from './assets/ic_multi-color--with-classes.svg';
+import classFromFillNote from '../notes/HdIconClassFromFill.md';
+import fillFromClassNote from '../notes/HdIconFillFromClass.md';
+import './styles/HdIcon.css';
 
 storiesOf('HdIcon', module)
   .add('simple', () => ({
@@ -13,12 +17,38 @@ storiesOf('HdIcon', module)
       };
     },
     template: `
-      <HdIcon
-        :src="icon"
-      />
+      <div>
+        <p>The <b>&lt;svg&gt;</svg></b> element is the root of the component,
+          so you can use its native attributes, like <b>width</b> and <b>height</b> in this example:</p>
+        <HdIcon
+          :src="icon"
+          width="64"
+          height="64"
+        />
+        <br>
+        <HdIcon
+          :src="icon"
+          width="128"
+          height="128"
+        />
+        <p>or <b>transform</b>:</p>
+        <HdIcon
+          :src="icon"
+          width="128"
+          height="128"
+          transform="rotate(90)"
+        />
+        <br>
+        <HdIcon
+          :src="icon"
+          width="128"
+          height="128"
+          transform="skewX(-10)"
+        />
+      </div>
     `,
   }), { percy: { skip: true } })
-  .add('multi-color', () => ({
+  .add('using classFromFill 📝', () => ({
     components: { HdIcon },
     data() {
       return {
@@ -27,10 +57,51 @@ storiesOf('HdIcon', module)
     },
     template: `
       <div>
+        <p>Check the notes for implementation details</p>
         <HdIcon
           :src="icon"
-          :colors="{'color-1': 'red', 'color-2': 'blue'}"
+          :class-from-fill="{ '#1895FF': 'secondary-color', '#1C3553': 'primary-color' }"
+          width="200"
+          height="200"
         />
+        <p>We add <b>primary-color</b> and <b>secondary-color</b> classes to the svg paths based on their fill color
+          (you can inspect the svg to make sure they were added successfully).</p><br>
+        <p>For demo purposes we change the fill of the secondary color on hover.</p>
       </div>
     `,
-  }), { percy: { skip: true } });
+  }), {
+    notes: {
+      markdown: classFromFillNote,
+    },
+    percy: {
+      skip: true,
+    },
+  })
+  .add('using fillFromClass 📝', () => ({
+    components: { HdIcon },
+    data() {
+      return {
+        icon: multiColorIconWithClasses,
+      };
+    },
+    template: `
+      <div>
+        <p>Check the notes for implementation details</p>
+        <HdIcon
+          :src="icon"
+          :fill-from-class="{ 'color-1': 'gray', 'color-2': 'rgb(200, 200, 200)' }"
+          width="200"
+          height="200"
+        />
+
+        <p>We change the fill color of the paths based on their class (originally defined in the icon's file)</p><br>
+      </div>
+    `,
+  }), {
+    notes: {
+      markdown: fillFromClassNote,
+    },
+    percy: {
+      skip: true,
+    },
+  });
